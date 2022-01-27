79c01caab7bc7c792a31e18e99450fe5bebe4b51
EC2 Default User <ec2-user@ip-172-31-82-191.ec2.internal>
2022-01-27 00:06:07 +0000

    my lab2.py file.

---
+++ lab2.py
@@ -0,0 +1,26 @@
+my_name = 'Tom' #START OF 3.1
+print(my_name.upper() )
+
+my_id = 123 #3.2
+print(my_id)
+
+#123 = my_id #3.3 
+#does not work because it cannot begin with a number, as it becomes a syntax error
+my_id = your_id = 123 # can be defined as it does not start with a number
+print(my_id) , print(your_id)
+
+my_id_str = '123'
+print(my_id_str)
+#print (type( '123' ) ) 
+
+#print(my_name + my_id) #3.5 does not work :(
+
+print(my_name + my_id_str) # 3.6
+
+print(my_name * 3) #3.7
+
+print('hello, world. This is my first python string.'.split('.')) #3.8
+
+message = "Tom's id is 123" #3.9
+print(message)
+
---
+++ lec2.py
@@ -0,0 +1,18 @@
+"""
+lec 2
+this is a regional comment
+"""
+# print("hello world") # this is a single line comment
+
+#print ( type( 123 ) )
+#print ( type( 123.) ) 
+#print (type( "123" ) ) 
+#print("Tom's cat")
+#print ("  Hello " + "World")
+#print(2 ** 3)
+my_int = 123
+print (my_int + 1)
+my_float = 123.
+my_str = 'hello world'
+print(my_float)
+print(my_str.upper())
